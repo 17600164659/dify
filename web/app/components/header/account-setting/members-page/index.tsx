dd6451e270ded6179f3b6d2e2028128d749adc34
NanWu <nanwu5522@gmail.com>
2023-06-08 13:11:19 +0800

    change ui

--- web/app/components/header/account-setting/members-page/index.tsx
+++ web/app/components/header/account-setting/members-page/index.tsx
@@ -35,24 +35,25 @@ const MembersPage = () => {
   const owner = accounts.filter(account => account.role === 'owner')?.[0]?.email === userProfile.email
   const { workspaces } = useWorkspacesContext()
   const currentWrokspace = workspaces.filter(item => item.current)?.[0]
-  
+
   return (
     <>
       <div>
         <div className='flex items-center mb-4 p-3 bg-gray-50 rounded-2xl'>
-          <div className={cn(s['logo-icon'], 'shrink-0')}></div>
-          <div className='grow mx-2'>
-            <div className='text-sm font-medium text-gray-900'>{currentWrokspace.name}</div>
-            <div className='text-xs text-gray-500'>{t('common.userProfile.workspace')}</div>
-          </div>
-          <div className='
+          {/* <div className={cn(s['logo-icon'], 'shrink-0')}></div> */}
+          <img src="https://assets.metaio.cc/assets/difyassets/logo.png" class="basic-sidebar-header" style={{ margin: 0 }}>
+            <div className='grow mx-2'>
+              <div className='text-sm font-medium text-gray-900'>{currentWrokspace.name}</div>
+              <div className='text-xs text-gray-500'>{t('common.userProfile.workspace')}</div>
+            </div>
+            <div className='
             shrink-0 flex items-center py-[7px] px-3 border-[0.5px] border-gray-200 
             text-[13px] font-medium text-primary-600 bg-white
             shadow-[0_1px_2px_rgba(16,24,40,0.05)] rounded-lg cursor-pointer
           ' onClick={() => setInviteModalVisible(true)}>
-            <UserPlusIcon className='w-4 h-4 mr-2 ' />
-            {t('common.members.invite')}
-          </div>
+              <UserPlusIcon className='w-4 h-4 mr-2 ' />
+              {t('common.members.invite')}
+            </div>
         </div>
         <div>
           <div className='flex items-center py-[7px] border-b border-gray-200'>
@@ -81,7 +82,7 @@ const MembersPage = () => {
                       owner && account.role !== 'owner'
                         ? <Operation member={account} onOperate={() => mutate()} />
                         : <div className='px-3 text-[13px] text-gray-700'>{RoleMap[account.role] || RoleMap.normal}</div>
-                    } 
+                    }
                   </div>
                 </div>
               ))
