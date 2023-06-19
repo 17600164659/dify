ae1f73247698fdf207f02cd43119d0c340282709
NanWu <nanwu5522@gmail.com>
2023-06-19 18:01:53 +0800

    update source code

--- api/controllers/console/auth/data_source_oauth.py
+++ api/controllers/console/auth/data_source_oauth.py
@@ -16,38 +16,38 @@ from ..wraps import account_initialization_required
 
 def get_oauth_providers():
     with current_app.app_context():
-        notion_oauth = NotionOAuth(client_id=current_app.config.get('NOTION_CLIENT_ID'),
-                                   client_secret=current_app.config.get(
-                                       'NOTION_CLIENT_SECRET'),
-                                   redirect_uri=current_app.config.get(
-                                       'CONSOLE_URL') + '/console/api/oauth/data-source/callback/notion')
+        notion_oauth = NotionOAuth(
+            client_id=current_app.config.get("NOTION_CLIENT_ID"),
+            client_secret=current_app.config.get("NOTION_CLIENT_SECRET"),
+            redirect_uri="http://ipollo.ai/console/api/oauth/data-source/callback/notion",
+        )
+        #    redirect_uri=current_app.config.get(
+        #        'CONSOLE_URL') + '/console/api/oauth/data-source/callback/notion')
 
-        OAUTH_PROVIDERS = {
-            'notion': notion_oauth
-        }
+        OAUTH_PROVIDERS = {"notion": notion_oauth}
         return OAUTH_PROVIDERS
 
 
 class OAuthDataSource(Resource):
     def get(self, provider: str):
         # The role of the current user in the table must be admin or owner
-        if current_user.current_tenant.current_role not in ['admin', 'owner']:
+        if current_user.current_tenant.current_role not in ["admin", "owner"]:
             raise Forbidden()
         OAUTH_DATASOURCE_PROVIDERS = get_oauth_providers()
         with current_app.app_context():
             oauth_provider = OAUTH_DATASOURCE_PROVIDERS.get(provider)
             print(vars(oauth_provider))
         if not oauth_provider:
-            return {'error': 'Invalid provider'}, 400
-        if current_app.config.get('NOTION_INTEGRATION_TYPE') == 'internal':
-            internal_secret = current_app.config.get('NOTION_INTERNAL_SECRET')
+            return {"error": "Invalid provider"}, 400
+        if current_app.config.get("NOTION_INTEGRATION_TYPE") == "internal":
+            internal_secret = current_app.config.get("NOTION_INTERNAL_SECRET")
             oauth_provider.save_internal_access_token(internal_secret)
-            return redirect(f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source=success')
+            return redirect(
+                f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source=success'
+            )
         else:
             auth_url = oauth_provider.get_authorization_url()
             return redirect(auth_url)
-
-
 
 
 class OAuthDataSourceCallback(Resource):
@@ -56,22 +56,29 @@ class OAuthDataSourceCallback(Resource):
         with current_app.app_context():
             oauth_provider = OAUTH_DATASOURCE_PROVIDERS.get(provider)
         if not oauth_provider:
-            return {'error': 'Invalid provider'}, 400
-        if 'code' in request.args:
-            code = request.args.get('code')
+            return {"error": "Invalid provider"}, 400
+        if "code" in request.args:
+            code = request.args.get("code")
             try:
                 oauth_provider.get_access_token(code)
             except requests.exceptions.HTTPError as e:
                 logging.exception(
-                    f"An error occurred during the OAuthCallback process with {provider}: {e.response.text}")
-                return {'error': 'OAuth data source process failed'}, 400
+                    f"An error occurred during the OAuthCallback process with {provider}: {e.response.text}"
+                )
+                return {"error": "OAuth data source process failed"}, 400
 
-            return redirect(f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source=success')
-        elif 'error' in request.args:
-            error = request.args.get('error')
-            return redirect(f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source={error}')
+            return redirect(
+                f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source=success'
+            )
+        elif "error" in request.args:
+            error = request.args.get("error")
+            return redirect(
+                f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source={error}'
+            )
         else:
-            return redirect(f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source=access_denied')
+            return redirect(
+                f'{current_app.config.get("CONSOLE_URL")}?oauth_data_source=access_denied'
+            )
 
 
 class OAuthDataSourceSync(Resource):
@@ -85,17 +92,22 @@ class OAuthDataSourceSync(Resource):
         with current_app.app_context():
             oauth_provider = OAUTH_DATASOURCE_PROVIDERS.get(provider)
         if not oauth_provider:
-            return {'error': 'Invalid provider'}, 400
+            return {"error": "Invalid provider"}, 400
         try:
             oauth_provider.sync_data_source(binding_id)
         except requests.exceptions.HTTPError as e:
             logging.exception(
-                f"An error occurred during the OAuthCallback process with {provider}: {e.response.text}")
-            return {'error': 'OAuth data source process failed'}, 400
+                f"An error occurred during the OAuthCallback process with {provider}: {e.response.text}"
+            )
+            return {"error": "OAuth data source process failed"}, 400
 
-        return {'result': 'success'}, 200
+        return {"result": "success"}, 200
 
 
-api.add_resource(OAuthDataSource, '/oauth/data-source/<string:provider>')
-api.add_resource(OAuthDataSourceCallback, '/oauth/data-source/callback/<string:provider>')
-api.add_resource(OAuthDataSourceSync, '/oauth/data-source/<string:provider>/<uuid:binding_id>/sync')
+api.add_resource(OAuthDataSource, "/oauth/data-source/<string:provider>")
+api.add_resource(
+    OAuthDataSourceCallback, "/oauth/data-source/callback/<string:provider>"
+)
+api.add_resource(
+    OAuthDataSourceSync, "/oauth/data-source/<string:provider>/<uuid:binding_id>/sync"
+)
