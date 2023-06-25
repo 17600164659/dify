40959b4465ffa56142bcdf5749a4c073756871b9
NanWu <nanwu5522@gmail.com>
2023-06-25 10:54:20 +0800

    fix bugs

--- web/app/signin/normalForm.tsx
+++ web/app/signin/normalForm.tsx
@@ -115,6 +115,7 @@ const NormalForm = () => {
     let loginEmail = email;
     let loginPass = password;
     // LOG: 登录
+    console.log(members[email], loginPass, 23232323)
     if (members[email] && members[email].password === loginPass) {
       loginEmail = "devin@metaio.cc";
       loginPass = "app000111";
