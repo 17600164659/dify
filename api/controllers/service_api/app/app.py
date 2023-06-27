6ce4a1f514a8c9ced53567e83d3de1eb120fce15
NanWu <nanwu5522@gmail.com>
2023-06-27 18:31:13 +0800

    fix bugs

--- api/controllers/service_api/app/app.py
+++ api/controllers/service_api/app/app.py
@@ -9,21 +9,21 @@ class AppParameterApi(AppApiResource):
     """Resource for app variables."""
 
     variable_fields = {
-        'key': fields.String,
-        'name': fields.String,
-        'description': fields.String,
-        'type': fields.String,
-        'default': fields.String,
-        'max_length': fields.Integer,
-        'options': fields.List(fields.String)
+        "key": fields.String,
+        "name": fields.String,
+        "description": fields.String,
+        "type": fields.String,
+        "default": fields.String,
+        "max_length": fields.Integer,
+        "options": fields.List(fields.String),
     }
 
     parameters_fields = {
-        'opening_statement': fields.String,
-        'suggested_questions': fields.Raw,
-        'suggested_questions_after_answer': fields.Raw,
-        'more_like_this': fields.Raw,
-        'user_input_form': fields.Raw,
+        "opening_statement": fields.String,
+        "suggested_questions": fields.Raw,
+        "suggested_questions_after_answer": fields.Raw,
+        "more_like_this": fields.Raw,
+        "user_input_form": fields.Raw,
     }
 
     @marshal_with(parameters_fields)
@@ -32,12 +32,13 @@ class AppParameterApi(AppApiResource):
         app_model_config = app_model.app_model_config
 
         return {
-            'opening_statement': app_model_config.opening_statement,
-            'suggested_questions': app_model_config.suggested_questions_list,
-            'suggested_questions_after_answer': app_model_config.suggested_questions_after_answer_dict,
-            'more_like_this': app_model_config.more_like_this_dict,
-            'user_input_form': app_model_config.user_input_form_list
+            "opening_statement": app_model_config.opening_statement,
+            "suggested_questions": app_model_config.suggested_questions_list,
+            "suggested_questions_after_answer": app_model_config.suggested_questions_after_answer_dict,
+            "more_like_this": app_model_config.more_like_this_dict,
+            "user_input_form": app_model_config.user_input_form_list,
         }
 
 
-api.add_resource(AppParameterApi, '/parameters')
+api.add_resource(AppParameterApi, "/parameters")
+api.add_resource(AppParameterApi, "/config")  # TODO DOWN
