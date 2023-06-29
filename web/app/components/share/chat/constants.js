1ca803f3e5c5fdeefa8d8865aa9908a4c94add1a
NanWu <nanwu5522@gmail.com>
2023-06-29 18:02:21 +0800

    remove sidebar

--- web/app/components/share/chat/constants.js
+++ web/app/components/share/chat/constants.js
@@ -1,40 +1,37 @@
 const pathnames = window.location.pathname.split('/')
 const id = pathnames[pathnames.length - 1];
-console.log(id, 23232323)
 export const decisionAppId = id === 'vEJdIfCYoHvn9peR' ? 'r3svUVKo2HJy6ncr' : 'HsvT57j5bktvP4sw';
 
-const roleList = [];
+export const roles = [];
 
 if (window.location.hostname === 'localhost') {
-    roleList.push({
+    roles.push({
         id,
         name: '测试AI',
         icon: 'https://assets.metaio.cc/assets/difyassets/web3zs.png'
     });
 } else if (window.location.hostname === 'gpt.metaio.cc') {
-    roleList.push({
+    roles.push({
         id,
         name: '测试AI',
         icon: 'https://assets.metaio.cc/assets/difyassets/web3zs.png'
     });
 } else {
-    roleList.push({
+    roles.push({
         id: 'vEJdIfCYoHvn9peR',
         name: 'Web3助手',
         icon: 'https://assets.metaio.cc/assets/difyassets/web3zs.png'
     });
-    roleList.push({
+    roles.push({
         id: '4BFHbBYoeX7hfNGv',
         name: '通用AI助手',
         icon: 'https://assets.metaio.cc/assets/difyassets/logo.png'
     });
     if (id !== 'vEJdIfCYoHvn9peR') {
-        roleList.push({
+        roles.push({
             id,
             name: '测试AI',
             icon: 'https://assets.metaio.cc/assets/difyassets/web3zs.png'
         });
     }
 }
-
-export const roles = roleList;
